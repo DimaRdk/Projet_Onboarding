5e8d79a05b323ded8f287650b19eb7b337c4c906
DimaRdk <dimitryrudenko@etu.utc.fr>
2024-02-08 11:02:40 +0100

    Git 2 python exec to test api

--- Projet_Onboarding/firstApi/urls.py
+++ Projet_Onboarding/firstApi/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import UserListAPIView, ProjectVersionAPIView , dashboard_view , change_version_view , project_version_view , project_user_view
+from rest_framework.authtoken import views
+
 app_name = "firstApi"
 urlpatterns = [
     
     
     path('', dashboard_view, name='dashboard'),
+    path('api-token-auth/', views.obtain_auth_token, name='api_token_auth'),
     path('disp-statistics', project_user_view, name='project-user'),
     path('disp-version/',project_version_view, name='disp-version'),
     path('statistics/', UserListAPIView.as_view(), name='user-list'),
