4e23dabdc131ed7e97b70be5e33a5f5c0cbfaae1
DimaRdk <dimitryrudenko@etu.utc.fr>
2024-02-09 12:57:14 +0100

    Add a model / AdminView for the Event Data

--- Projet_Onboarding/firstApi/admin.py
+++ Projet_Onboarding/firstApi/admin.py
@@ -1,5 +1,33 @@
 from django.contrib import admin
-from .models import ProjectVersion
-
+from .models import ProjectVersion, Event 
+from django.contrib.auth.models import User
 
 admin.site.register(ProjectVersion)
+
+class FeaturesAdmin(admin.ModelAdmin):
+    # Cette ligne détermine quels champs sont affichés dans la vue liste
+    list_display = ('name',) # Assurez-vous que ces champs existent dans votre modèle
+
+class CategoryAdmin(admin.ModelAdmin):
+    # Idem pour Category
+    list_display = ('name',) # Assurez-vous que ces champs existent dans votre modèle
+
+
+
+   
+class EventAdmin(admin.ModelAdmin):
+        
+        actions = ['delete_selected']
+
+        list_display = ('user', 'user_group', 'event', 'created', 'feature', 'action_type')
+        list_filter = ('user', 'user_group', 'event', 'feature', 'action_type')
+        search_fields = ('user__username', 'user_group', 'event', 'feature', 'action_type')
+        date_hierarchy = 'created'
+    
+        def formfield_for_foreignkey(self, db_field, request, **kwargs):
+            if db_field.name == 'user':
+                kwargs['empty_label'] = '(None)'
+                kwargs['queryset'] = User.objects.all()
+            return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+admin.site.register(Event, EventAdmin)
--- Projet_Onboarding/firstApi/models.py
+++ Projet_Onboarding/firstApi/models.py
@@ -1,9 +1,35 @@
 from django.db import models
-
-
+from datetime import datetime
+from django.contrib.auth.models import User
 
 class ProjectVersion (models.Model):
     version = models.CharField(max_length=10)
     def __str__(self) -> str:
         return self.version
+
+
+    
+    
+class Event(models.Model):
+    user= models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user_group = models.CharField(max_length=255)
+    event = models.CharField(max_length=255)
+    created = models.DateTimeField()
+    userinfo = models.CharField(max_length=255, null=True, blank=True)
+    feature = models.CharField(max_length=255)
+    action_type = models.CharField(max_length=50, choices=[
+                ('CREATE', 'CREATE'),
+                ('DELETE', 'DELETE'),
+                ('MODIF', 'MODIF'),
+                ('ASKMEYA', 'ASKMEYA'),
+                ('NAVIGATE', 'NAVIGATE'),
+                ('CHOOSE', 'CHOOSE'),
+                ('DISCONNECT', 'DISCONNECT')
+            ])
+
+    def __str__(self):
+        return f"{self.user} - {self.event} - {self.created}"
+
+
+    
 
